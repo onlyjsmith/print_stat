4d9d7e3e5c636b8dca92775c685c03ce708269a8
Jonathan Smith <jonathan@impacti.org>
2011-10-27 09:56:09 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintStat::Application.config.secret_token = 'dc8d0e60b44620c25cb9d80c47247a6a5d050700d292d446bdf84df0241292dfaf6e27f937aa060c0a2cccf8edd3342a16becf3da4d5cb78707efcf5c5e6c6e4'
